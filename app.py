97e04a13517b131bd38cf74260890548594f1148
Hardy Hardytati Tati <hardytati@Hardys-MacBook-Air.local>
2023-10-15 22:24:48 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## vectorize the data and store in db 
+
+loader = CSVLoader(file_path="ecomBookExtract4.csv")
+documents = loader.load()
+
+print(len(documents))
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
+
+## carry out similarity search
+
+def retrieve_info(query):
+    similar_response = db.similarity_search(query, k=3)
+
+    page_contents_array = [doc.page_content for doc in similar_response]
+
+    # print(page_contents_array)
+
+    return page_contents_array
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+
+template = """
+You are a world-class librarian, who is able to share very relevant book recommendations when library patrons come to you with a particular topic that they want to find relevant books for. 
+I will share a query from a library patron, and you will give me the most relevant book that I should send to them. 
+Below is the query received from the library patron:
+{message}
+
+Here is a list of books in our corpus. Please recommend a book from this list:
+{best_practice}
+
+Please write the best response that I should send to this prospect:
+"""
+
+prompt = PromptTemplate(
+    input_variables=["message", "best_practice"],
+    template=template
+)
+
+chain = LLMChain(llm=llm, prompt=prompt)
+
+
+## Set up LLMChain and prompts
+
+def generate_response(message):
+    best_practice = retrieve_info(message)
+    response = chain.run(message=message, best_practice=best_practice)
+    return response 
+
+
+
+# 5. Build an app with streamlit
+def main():
+    st.set_page_config(
+        page_title="Customer response generator", page_icon=":bird:")
+
+    st.header("Customer response generator :bird:")
+    message = st.text_area("customer message")
+
+    if message:
+        st.write("Generating best practice message...")
+
+        result = generate_response(message)
+
+        st.info(result)
+
+
+if __name__ == '__main__':
+    main()
